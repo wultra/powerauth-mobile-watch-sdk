39bd6ecbcf915dc031a22dfe489e76ff63ffdc5f
Juraj Ďurech <durech.juraj@gmail.com>
2026-05-21 10:34:30 +0200

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "PowerAuthForWatch",
+    platforms: [
+        .watchOS(.v4)
+    ],
+    products: [
+        .library(name: "PowerAuth2ForWatch", type: .dynamic, targets: ["PowerAuth2ForWatch"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/wultra/cc7", exact: "0.7.0-rc2")
+    ],
+    targets: [
+        
+        // --- PowerAuth2ForWatch ---
+        
+        .target(
+            name: "PowerAuth2ForWatch",
+            dependencies: [
+                .product(name: "openssl", package: "cc7")
+            ],
+            path: "Sources",
+            exclude: [
+                "PowerAuth2ForWatch/Info.plist",
+                "PowerAuth2ForWatch/PowerAuth2ForWatch.modulemap"
+            ],
+            sources: [
+                "PowerAuth2ForWatch",
+                "PowerAuth2ForWatchPrivate",
+            ],
+            publicHeadersPath: "PowerAuth2ForWatch",
+            cSettings: [
+                // Required for "Header.h" style imports
+                .headerSearchPath("PowerAuth2ForWatch"),
+                .headerSearchPath("PowerAuth2ForWatchPrivate"),
+                .define("PA2_EXTENSION_SDK", to: "1"),
+                .define("PA2_WATCH_SDK", to: "1")
+            ]
+        )
+    ],
+    cLanguageStandard: .c17,
+    cxxLanguageStandard: .cxx20
+)
